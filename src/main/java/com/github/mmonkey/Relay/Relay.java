55fdf776f3569afbbfa18402148c0eb1c60bd1d7
mmonkey <mmonkey@gmail.com>
2015-05-02 22:57:17 -0500

    Added basic getters and setters for plugin class

--- src/main/java/com/github/mmonkey/Relay/Relay.java
+++ src/main/java/com/github/mmonkey/Relay/Relay.java
@@ -1,5 +1,46 @@
 package com.github.mmonkey.Relay;
 
+import org.slf4j.Logger;
+import org.spongepowered.api.Game;
+import org.spongepowered.api.event.Subscribe;
+import org.spongepowered.api.event.state.PreInitializationEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.plugin.PluginContainer;
+
+import com.google.common.base.Optional;
+
+@Plugin(id = Relay.ID, name = Relay.NAME, version = Relay.VERSION)
 public class Relay {
+	
+	public static final String ID = "Relay";
+	public static final String NAME = "Relay";
+	public static final String VERSION = "0.0.1";
+	
+	private Game game;
+	private Optional<PluginContainer> pluginContainer;
+	private static Logger logger;
+	
+	public Game getGame() {
+		return this.game;
+	}
+	
+	public Optional<PluginContainer> getPluginContainer() {
+		return this.pluginContainer;
+	}
+	
+	public static Logger getLogger() {
+		return logger;
+	}
+	
+	@Subscribe
+	public void onPreInit(PreInitializationEvent event) {
+		
+		this.game = event.getGame();
+		this.pluginContainer = game.getPluginManager().getPlugin(Relay.NAME);
+		Relay.logger = game.getPluginManager().getLogger(pluginContainer.get());
+		
+		getLogger().info(String.format("Starting up %s v%s.", Relay.NAME, Relay.VERSION));
+			
+	}
 
 }
