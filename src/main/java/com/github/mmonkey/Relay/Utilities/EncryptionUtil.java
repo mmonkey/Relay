2679d6e1c4a248d129117a5f5b20701c81fbdaa2
mmonkey <mmonkey@gmail.com>
2015-05-02 22:54:36 -0500

    Added AES EncryptionUtil for storing emails and phone numbers

---
+++ src/main/java/com/github/mmonkey/Relay/Utilities/EncryptionUtil.java
@@ -0,0 +1,45 @@
+package com.github.mmonkey.Relay.Utilities;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class EncryptionUtil {
+
+	private String encryptionKey;
+
+    public EncryptionUtil(String encryptionKey)
+    {
+        this.encryptionKey = encryptionKey;
+    }
+
+    public String encrypt(String plainText) throws Exception
+    {
+        Cipher cipher = getCipher(Cipher.ENCRYPT_MODE);
+        byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
+
+        return Base64.encodeBase64String(encryptedBytes);
+    }
+
+    public String decrypt(String encrypted) throws Exception
+    {
+        Cipher cipher = getCipher(Cipher.DECRYPT_MODE);
+        byte[] plainBytes = cipher.doFinal(Base64.decodeBase64(encrypted));
+
+        return new String(plainBytes);
+    }
+
+    private Cipher getCipher(int cipherMode)
+            throws Exception
+    {
+        String encryptionAlgorithm = "AES";
+        SecretKeySpec keySpecification = new SecretKeySpec(
+                encryptionKey.getBytes("UTF-8"), encryptionAlgorithm);
+        Cipher cipher = Cipher.getInstance(encryptionAlgorithm);
+        cipher.init(cipherMode, keySpecification);
+
+        return cipher;
+    }
+    
+}
