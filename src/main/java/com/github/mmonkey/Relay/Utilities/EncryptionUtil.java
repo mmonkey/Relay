96ba7aa3c6bc9892e1731253e1f3a7104fb6916c
mmonkey <mmonkey@gmail.com>
2015-05-04 22:29:07 -0500

    Fixed EncryptionUtils, updated generateSecretKey() method

--- src/main/java/com/github/mmonkey/Relay/Utilities/EncryptionUtil.java
+++ src/main/java/com/github/mmonkey/Relay/Utilities/EncryptionUtil.java
@@ -1,69 +1,59 @@
 package com.github.mmonkey.Relay.Utilities;
 
 import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
 import java.security.GeneralSecurityException;
 import java.util.Random;
 
 import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
 
 public class EncryptionUtil {
 	
-	private String encryptionKey;
+	private String secretKey;
 	
-	public EncryptionUtil(String key) {
-		this.encryptionKey = key;
+	public EncryptionUtil(String secretKey) {
+		this.secretKey = secretKey;
 	}
 
 	public String encrypt(String plainText) throws GeneralSecurityException, UnsupportedEncodingException {
 		
-		byte[] raw = encryptionKey.getBytes(Charset.forName("UTF-8"));
-
-		if (raw.length != 16) {
-			throw new IllegalArgumentException("Invalid key size.");
-		}
-
-		SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-		cipher.init(Cipher.ENCRYPT_MODE, skeySpec, new IvParameterSpec(new byte[16]));
+		byte[] text = plainText.getBytes();
+		byte[] raw = secretKey.getBytes();
+		SecretKeySpec keySpec = new SecretKeySpec(raw, "AES");
 		
-		byte[] encrypted = cipher.doFinal(plainText.getBytes(Charset.forName("UTF-8")));
-		return new String(encrypted, Charset.forName("UTF-8"));
+		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+		
+		byte[] encryptedText = cipher.doFinal(text);
+		return Base64.encodeBase64String(encryptedText);
 		
 	}
 
-	public String decrypt(String encryptedText) throws GeneralSecurityException {
-		
-		byte[] encrypted = encryptedText.getBytes(Charset.forName("UTF-8"));
-		byte[] raw = encryptionKey.getBytes(Charset.forName("UTF-8"));
+	public String decrypt(String encryptedText) throws GeneralSecurityException, UnsupportedEncodingException {
 
-		if (raw.length != 16) {
-			throw new IllegalArgumentException("Invalid key size.");
-		}
+		byte[] encryptText = Base64.decodeBase64(encryptedText);
+		SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(), "AES");
 		
-		SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+		cipher.init(Cipher.DECRYPT_MODE, keySpec);
 		
-		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-		cipher.init(Cipher.DECRYPT_MODE, skeySpec, new IvParameterSpec(new byte[16]));
-		
-	    byte[] decrypted = cipher.doFinal(encrypted);
-	    return new String(decrypted, Charset.forName("UTF-8"));
+	    return new String(cipher.doFinal(encryptText));
 	    
 	}
     
 	public static String generateSecretKey() {
 		
-		Random r = new Random();
-		StringBuilder sb = new StringBuilder();
+		Random rand = new Random();
+		String characters = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ0123456789";
+		char[] key = new char[16];
 		
 		for(int i = 0; i < 16; i++) {
-			char c = (char) ((int)r.nextInt(128));
-			sb.append(c);
+			key[i] = characters.charAt(rand.nextInt(characters.length()));
 		}
 		
-		return sb.toString();
+		return new String(key);
 	
 	}
 	
