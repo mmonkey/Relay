1b180e9e6789abadb1d7f02e234f2647a8bbcbe1
mmonkey <mmonkey@gmail.com>
2015-05-09 16:59:38 -0500

    Added register event subcommand

---
+++ src/main/java/com/github/mmonkey/Relay/Commands/RegisterEditSubCommand.java
@@ -0,0 +1,11 @@
+package com.github.mmonkey.Relay.Commands;
+
+import com.github.mmonkey.Relay.Relay;
+
+public class RegisterEditSubCommand extends RegisterCommand {
+
+	public RegisterEditSubCommand(Relay plugin) {
+		super(plugin);
+	}
+
+}
